7b9d20dd889e1e466f6d6acbcde1bd9904023b8e
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-24 19:03:57 +0600

    A Simple calculator using Dart Variables

--- Module 01/variables.dart
+++ Module 01/variables.dart
@@ -1,4 +1,27 @@
+import 'dart:io';
 void main ()
 {
-  print("Hello World");
+  // Simple calculator using variable concept
+
+
+  var firstNumber  = 50;
+  print("Your First Number is = $firstNumber");
+  print("\n");
+
+  var secondNumber = 30;
+  print("Your Second Number is = $secondNumber");
+
+  var  sum = firstNumber + secondNumber;
+  var  sub = firstNumber - secondNumber;
+  var  mul = firstNumber * secondNumber;
+  var  div = firstNumber / secondNumber;
+  var  mod = firstNumber % secondNumber;
+
+  print("Summation is      = $sum");
+  print("Subtraction is    = $sub");
+  print("Multiplication is = $mul");
+  print("Division is        = $div");
+  print("Modulus is        = $mod");
+
+
 }
