164e7b3be723004d684e167672c5000c90ebcd5d
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-26 18:29:22 +0600

    Module 01 Live test practise

---
+++ Module 01/Assignment 01 Practice.dart
@@ -0,0 +1,14 @@
+void main ()
+{
+   var studentmap = {
+     'StudentID':1903203,
+           'Age':24,
+        'Address':'Narayanganj',
+         'Class':15,
+        'Gender':'Male',
+   };
+
+   var studentAddress = studentmap['Address'];
+   print("Address $studentAddress");
+
+}
