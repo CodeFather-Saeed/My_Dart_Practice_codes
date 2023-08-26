1b69a6f381ed642778518a7b6d4e3b69fbe8a326
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-27 00:32:27 +0600

    Module 02 Video 28

---
+++ Module 02/Switch Case Statement .dart
@@ -0,0 +1,29 @@
+void main () {
+  var marks = 45;
+
+  switch (marks) {
+    case 80:
+      print("Result is A+ Grade");
+      break;
+
+    case 70:
+      print("Result is A Grade");
+      break;
+
+    case 60:
+      print("Result is A- Grade");
+      break;
+
+    case 50:
+      print("Result is B Grade");
+      break;
+
+    case 40:
+      print("Result is C Grade");
+      break;
+
+    default:
+      print("Result is not found");
+      break;
+  }
+}
