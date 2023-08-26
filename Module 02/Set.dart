68b511fe2149dac51c12e437ad0c3b54d77423ab
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-26 22:13:30 +0600

    Module 02 video 21

--- Module 02/Set.dart
+++ Module 02/Set.dart
@@ -1,18 +1,27 @@
 void main()
 {
-  var mycityName =<String>{"Kazi" , "Saeed","Hossain"}; // initialize a set
+  var mycityName =<String>{"Kazi" , "Saeed","Hossain"}; // Initialized a set
   print(mycityName);
 
-  mycityName.add("Naeem");      // added only one value in set
+  mycityName.add("Naeem");      // Added only one value in set
   print(mycityName);
 
-  mycityName.addAll({"Sadman","Sakib","Jisan"}); // added multiple values in set
+  mycityName.addAll({"Sadman","Sakib","Jisan"}); // Added multiple values in set
   print(mycityName);
 
   var x = mycityName.elementAt(4);
   print("At the index of 2 the element is $x");
   // Show the element of the index
 
-  mycityName.clear();    // clear all the element of set
-  print(mycityName);
+ // mycityName.clear();    // Clear all the element of set
+ // print(mycityName);
+
+
+  print(mycityName.first);     // To shows the first element in the set
+  print(mycityName.last);      // To shows the last element in the set
+  print(mycityName.length);    // To shows the length of the set
+  print(mycityName.isEmpty);   // to shows Is the set empty ?
+  print(mycityName.isNotEmpty);  // to shows Isn't the set empty ?
+  print(mycityName.single);      // It is use when the set have only one element
+  print(mycityName.hashCode);
 }
