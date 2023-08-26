6ba6a24702eea97700f9f3f758cd28598d347c42
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-26 22:33:47 +0600

    Module 02 video 22

---
+++ Module 02/Map Literal.dart
@@ -0,0 +1,14 @@
+void main ()
+{
+  var student = {
+
+    'Name': 'Kazi Saeed Hossain',
+    'Age': 25,
+    'City': 'Dhaka',
+  };
+  print(student);
+
+  student ['Country'] = 'Bangladesh';
+  student ['University'] = 'Monash University, Australia';
+  print(student);
+}
