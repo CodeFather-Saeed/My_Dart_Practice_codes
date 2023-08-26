e94d5de708c03f88e034ae1291e08b8e4405a242
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-26 22:50:09 +0600

    Module 02 video 23

---
+++ Module 02/Map Constractor.dart
@@ -0,0 +1,19 @@
+void main ()
+{
+  var Student = new Map ();
+
+  Student ['Name'] = 'Saeed';
+  Student ['Age'] = 25;
+  Student ['City'] = 'Dhaka';
+
+  print(Student);
+
+  Student ['Country'] ='Bangladesh';
+  print(Student);
+
+  print(Student.keys);    // To shows all keys of the Map
+  print(Student.values);   // To shows all values of the Map
+  print(Student.length);   // To shows the length of the Map
+  print(Student.isEmpty);    // To shows Is the Map empty ?
+  print(Student.isNotEmpty);  //// To shows Isn't the Map empty ?
+}
