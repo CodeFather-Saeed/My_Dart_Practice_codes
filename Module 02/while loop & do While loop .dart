83f8a6fe373a5a50cc1229155652aede88ac99ad
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-27 12:52:42 +0600

    Module 02 video 31

---
+++ Module 02/while loop & do While loop .dart
@@ -0,0 +1,27 @@
+void main (){
+
+  /*------------------for loop Syntax----------------
+  for(var i = 0; i<=100; i = i+1){
+
+      print(i);
+   };
+  */
+
+  /*-------------------while loop Syntax------------------------
+  var i = 0;
+  while(i<=100){
+
+      print("While loop = $i");
+      i = i+1;
+    };
+  */
+
+  //-----------------------do while loop Syntax-------------------
+
+  var i = 0;
+  do{
+    print("do while loop = $i");       ///
+    i = i+5;
+  }while(i<=100);
+
+}
