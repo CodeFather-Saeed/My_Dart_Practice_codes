8af096764f035686de3bdf6ae8fd6f186cd34504
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-27 11:58:59 +0600

    Module 02 Video 29 and 30

--- Module 02/For Loop .dart
+++ Module 02/For Loop .dart
@@ -1,7 +1,27 @@
 void main ()
 {
-  for(var i =0; i<=10000 ; i = i+5){
 
-    print(i);
+  //----------------For Loop--------------------------
+  for(var i =1; i<=10000 ; i = i+1){
+
+    print("Subahan Allah = "+i.toString());
+    print("Alhamdolillah = "+i.toString());
+    print("laailaha Illah = "+i.toString());
+    print("Allahu Akbar = "+i.toString());
+    print("\n");
   }
+
+  /*-----------------for loop in list------------------
+  var alphabetlist = ['A','B','C','D','E','F'];
+
+  for(var oneAlphabet in alphabetlist){
+    print(oneAlphabet);
+  }*/
+
+  /*-----------------for loop in Set------------------
+  var alphabetSet = {'A','B','C','D','E','F'};
+
+  for(var oneAlphabet in alphabetSet){
+    print(oneAlphabet);
+  }*/
 }
